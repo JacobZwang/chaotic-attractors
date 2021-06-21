1357b06d50c47c5e183e9b29a3fca7ba24631fff
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2021-06-21 16:04:53 -0700

    attempt lines

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
     );
 
     const renderer = new THREE.WebGLRenderer();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
@@ -26,33 +27,53 @@ function App() {
     const geometry = new THREE.BufferGeometry();
     // const material = new THREE.PointsMaterial({ color: 0x00ff00 });
 
-    let material = new THREE.ShaderMaterial({
-      transparent: true,
-      uniforms: {
-        size: { value: 10 },
-        scale: { value: 1 },
-        color: { value: new THREE.Color("maroon") },
-      },
-      vertexShader: THREE.ShaderLib.points.vertexShader,
-      fragmentShader: `
-      uniform vec3 color;
-      void main() {
-          vec2 xy = gl_PointCoord.xy - vec2(0.15);
-          float ll = length(xy);
-          gl_FragColor = vec4(color, step(ll, 0.15));
-      }
-      `,
-    });
+    // let material = new THREE.ShaderMaterial({
+    //   transparent: true,
+    //   uniforms: {
+    //     size: { value: 20 },
+    //     scale: { value: 1 },
+    //     color: { value: new THREE.Color("maroon") },
+    //   },
+    //   vertexShader: THREE.ShaderLib.points.vertexShader,
+    //   fragmentShader: `
+    //   uniform vec3 color;
+    //   void main() {
+    //       vec2 xy = gl_PointCoord.xy - vec2(0.5);
+    //       float ll = length(xy);
 
-    const vertices = new Float32Array(
-      Array.from({ length: 30000 }, () => (Math.random() * 2) / 100)
-    );
+    //       gl_FragColor = vec4(color, step(ll, 0.5) > 0.0);
+    //   }
+    //   `,
+    // });
 
-    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+    // const numParticles = 50;
+    // const trailLength = 1000;
+
+    // const vertices = new Float32Array(
+    //   Array.from(
+    //     { length: numParticles * trailLength * 3 },
+    //     () => (Math.random() * 2) / 100
+    //   )
+    // );
+
+    // geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
     // geometry.computeBoundingSphere();
 
-    let points = new THREE.Points(geometry, material);
-    scene.add(points);
+    // let points = new THREE.Points(
+    //   geometry,
+    //   new THREE.PointsMaterial({ color: "maroon", size: 0.5 })
+    // );
+    // scene.add(points);
+
+    let lines = Array.from({ length: 10 }).map(() =>
+      new Array(100).fill(
+        new THREE.Vector3(
+          (Math.random() * 2) / 100,
+          (Math.random() * 2) / 100,
+          (Math.random() * 2) / 100
+        )
+      )
+    );
 
     camera.position.z = -70;
     camera.position.x = -70;
@@ -62,24 +83,76 @@ function App() {
     const b = 28;
     const c = 8 / 3;
 
-    function animate(t: number) {
-      for (let i = 0; i < vertices.length; i += 3) {
-        const x = vertices[i];
-        const y = vertices[i + 1];
-        const z = vertices[i + 2];
+    const increment = 0.00001;
+    const start = 0.001;
 
-        vertices[i] = x + t * a * (y - x);
-        vertices[i + 1] = y + t * (x * (b - z) - y);
-        vertices[i + 2] = z + t * (x * y - c * z);
+    const material = new THREE.LineBasicMaterial({ color: "maroon" });
+
+    function animate(t: number) {
+      while (scene.children.length > 0) {
+        scene.remove(scene.children[0]);
       }
 
-      geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+      lines = lines.map((line) =>
+        line.map((point, i) => {
+          const x = point.x;
+          const y = point.y;
+          const z = point.z;
 
-      requestAnimationFrame(() => animate(t + 0.00001));
+          let ti = t + i / t;
+
+          let xi = x + ti * a * (y - x);
+          let yi = y + ti * (x * (b - z) - y);
+          let zi = z + ti * (x * y - c * z);
+
+          return new THREE.Vector3(xi, yi, zi);
+        })
+      );
+
+      lines.forEach((line) => {
+        const buffer = new THREE.BufferGeometry().setFromPoints(line);
+        const geometry = new THREE.Line(buffer, material);
+        scene.add(geometry);
+      });
+
+      // for each particle
+      // for (let i = 0; i < vertices.length; i += 30) {
+      //   const x = vertices[i];
+      //   const y = vertices[i + 1];
+      //   const z = vertices[i + 2];
+
+      //   // for each trail particle
+      //   for (let j = 0; j < trailLength; j += 1) {
+      //     let ti = t + j * (increment / 10000000);
+
+      //     vertices[i + j * 3] = x + ti * a * (y - x);
+      //     vertices[i + 1 + j * 3] = y + ti * (x * (b - z) - y);
+      //     vertices[i + 2 + j * 3] = z + ti * (x * y - c * z);
+      //   }
+      // }
+
+      // for (let i = 0; i < vertices.length / (trailLength * 3); i += 1) {
+      //   for (let j = 0; j < trailLength; j += 1) {
+      //     // let j = 0;
+      //     let ti = t + j * increment;
+
+      //     const x = vertices[i * trailLength * 3];
+      //     const y = vertices[i * trailLength * 3 + 1];
+      //     const z = vertices[i * trailLength * 3 + 2];
+
+      //     vertices[i * trailLength * 3 + j] = x + ti * a * (y - x);
+      //     vertices[i * trailLength * 3 + 1 + j] = y + ti * (x * (b - z) - y);
+      //     vertices[i * trailLength * 3 + 2 + j] = z + ti * (x * y - c * z);
+      //   }
+      // }
+
+      // geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+      requestAnimationFrame(() => animate(t + increment));
       controls.update();
       renderer.render(scene, camera);
     }
-    animate(0.001);
+    animate(start);
   });
   return <div id="three-container"></div>;
 }
